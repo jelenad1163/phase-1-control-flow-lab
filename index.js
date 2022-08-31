9d6223cab372a85129ebf599796a37797b4a262e
Jelena Djurdjevic <+>
2022-08-30 20:41:35 -0600

    commit tree 4b4bbfd5ff283f9deff6db6c73089c600532320d

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let freeRide = 'This one is on me!';
+  if (someValue <= 400){
+    return freeRide;
+  }
+  else if (someValue <= 2000){
+    return 'That will be twenty bucks.';
+  }
+   else if (someValue <=2500){
+    return 'I will gladly take your thirty bucks.'; 
+  }
+    else {
+     return 'No can do.';
+    }
+  }
+  
+function ternaryCheckCity(value){
+  return value === 'NYC' ? "Ok, sounds good." : "No go.";
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(greeting){
+  switch (greeting){
+    case 'generous':
+      return "Thank you so much.";
+      case 'not as generous':
+        return "Thank you.";
+        default:
+          return "Bye."
+          
+  }
+  
 }
